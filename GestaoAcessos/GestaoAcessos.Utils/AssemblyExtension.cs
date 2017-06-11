d7827918c6be57453e6042bb593769c8ac6c1f44
André Rocha Agostinho <Andr Rocha Agostinho>
2017-06-11 16:17:55 -0300

    Extension methods para assembly

---
+++ GestaoAcessos/GestaoAcessos.Utils/AssemblyExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+
+    public static class AssemblyExtensionMethods
+    {
+        public static Assembly GetAssembly(this String typeName)
+        {
+            Assembly _assemnbly = AppDomain.CurrentDomain.GetAssemblies().Where(p => p.FullName.Contains(typeName)).FirstOrDefault();
+            return _assemnbly;
+        }
+
+    }
+}
