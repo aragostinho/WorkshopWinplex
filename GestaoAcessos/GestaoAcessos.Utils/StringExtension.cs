ef8ae8ef3e2728915afcbcd3ee4875dceec620d2
aragostinho <andre@magnadev.com.br>
2017-06-11 22:00:41 -0300

    Extensions para strings

---
+++ GestaoAcessos/GestaoAcessos.Utils/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class StringExtension
+    {
+
+        public static bool IsNullOrEmpty(this string pString)
+        {
+            return string.IsNullOrEmpty(pString);
+        }
+         
+    }
+}
