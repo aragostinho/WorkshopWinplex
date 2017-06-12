41f660c7824fd60a077daae5b4ab65c4af9978c6
aragostinho <andre@magnadev.com.br>
2017-06-11 22:20:15 -0300

    Session Factory

---
+++ GestaoAcessos/GestaoAcessos.Business/Factories/SessionFactory.cs
@@ -0,0 +1,23 @@
+﻿using GestaoAcessos.Infra;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoAcessos.Business.Factories
+{
+    public static class SessionFactory
+    {
+        public static ISession Open()
+        {
+            return FluentNHibernateHelper.GetSession;
+        }
+
+        public static void Close()
+        {
+            FluentNHibernateHelper.CloseSession();
+        }
+    }
+}
